6b3e411976a222e6a641a9321785649dc8f913c0
kotathefriends <5070kouta@gmail.com>
2023-10-17 20:25:23 +0900

    Initial Commit

---
+++ QuizSound/QuizSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizSoundApp.swift
+//  QuizSound
+//
+//  Created by KOTA TAKAHASHI on 2023/10/17.
+//
+
+import SwiftUI
+
+@main
+struct QuizSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
